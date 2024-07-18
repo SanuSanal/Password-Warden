7ba8191f824a010f2923f857d1dd9a2077cf3bf1
SanuSanal <=>
2024-07-18 16:23:44 +0100

    key retrieval issue fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:password_warden/models/password_record.dart';
 import 'package:password_warden/screens/splash_screen_page.dart';
@@ -11,7 +12,24 @@ void main() async {
 
   const FlutterSecureStorage secureStorage = FlutterSecureStorage();
   const String encryptionKey = 'encryptionKey';
-  String? encryptedKey = await secureStorage.read(key: encryptionKey);
+
+  String? encryptedKey;
+  try {
+    encryptedKey = await secureStorage.read(key: encryptionKey);
+  } catch (e) {
+    await secureStorage.delete(key: encryptionKey);
+    encryptedKey = null;
+
+    Fluttertoast.showToast(
+        msg:
+            "An error occurred and all previous data was lost. A new encryption key has been generated.",
+        toastLength: Toast.LENGTH_LONG,
+        gravity: ToastGravity.BOTTOM,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.red,
+        textColor: Colors.white,
+        fontSize: 16.0);
+  }
 
   if (encryptedKey == null) {
     final key = Hive.generateSecureKey();
